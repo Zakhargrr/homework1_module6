5d23ae9cd79e4f74d0ebb8b702a377d422d181a4
zakhar <grankin-zakhar@mail.ru>
2023-08-15 18:56:26 +0700

    init

---
+++ coursework_module5/models/api_class.py
@@ -0,0 +1,16 @@
+import requests
+
+
+class HeadHunterAPI:
+
+    def __init__(self):
+        self.__vacancies = None
+        self.__status_code = None
+
+    def get_vacancies(self, employer_id, page):
+        vacancies = requests.get(f'https://api.hh.ru/vacancies?employer_id={employer_id}&per_page=100&page={page}')
+        self.__vacancies = vacancies.json()
+        self.__status_code = vacancies.status_code
+        return self.__vacancies, self.__status_code
+
+
